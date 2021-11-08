7eaeac602834ba136c7809f918c89a6a6cb68c53
Benna96 <vissersofia@gmail.com>
2021-11-08 12:30:01 +0200

    Delete Generate.bat
    This is just for dev

--- ELE_Patcher/FormKeys/Generate.bat
+++
@@ -1,4 +0,0 @@
-set theExe=D:\Coding\GitHub\Mutagen.Bethesda.FormKeys\Mutagen.Bethesda.FormKeys.Generator\bin\Debug\net5.0\Mutagen.Bethesda.FormKeys.Generator.exe
-set modPath=C:\Games\MO2\mods\Medieval Lanterns of Skyrim\Medieval Lanterns of Skyrim.esp
-%theExe% generate -g SkyrimSE -p "%modPath%"
-pause
